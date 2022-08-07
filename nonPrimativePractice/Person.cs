80c78d416cdbbc50d437bd6cb4b7002afd27356d
Sonny Icks <sicks1@uco.edu>
2022-08-07 15:18:33 -0500

    Add project files.

---
+++ nonPrimativePractice/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace nonPrimativePractice
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + firstName + " " + lastName);
+        }
+    }
+}
---
+++ nonPrimativePractice/Program.cs
@@ -0,0 +1,22 @@
+﻿using nonPrimativePractice.Math;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nonPrimativePractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sonny = new Person();
+            sonny.firstName = "Sonny";
+            sonny.lastName = "Icks";
+            sonny.Introduce();
+            var calc1 = new Calculator();
+            var sum = calc1.Add(1, 2);
+            System.Console.WriteLine(sum);
+        }
+    }
+}
